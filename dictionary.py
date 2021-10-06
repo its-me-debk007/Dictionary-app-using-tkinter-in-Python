1650bd49806c300fa9c86a4bcce73ae4566904bf
its-me-debk007 <debashish.joy@gmail.com>
2021-10-06 12:09:57 +0530

    Made exe file of the app. Also changed the background of the app

--- dictionary.py
+++ dictionary.py
@@ -2,20 +2,19 @@ import requests
 from tkinter import *
 from PIL import Image,ImageTk
 from tkhtmlview import HTMLLabel
-from playsound import playsound
 from PyDictionary import PyDictionary
 
 window=Tk()
 window.title("Dictionary")
-window.iconbitmap("icon.ico")
+window.iconbitmap("C:\\Users\\pc\\Documents\\GitHub\\Dictionary-app-using-tkinter-in-Python\\icon.ico")
 window.configure(bg= "black")
-window.geometry("920x390")
-# window.resizable(False, False)
+window.geometry("920x385")
+window.resizable(False, False)
 
-# img=ImageTk.PhotoImage(Image.open("image.jpg"))
-# Label(window,image=img).pack(side= RIGHT)
+img=ImageTk.PhotoImage(Image.open("C:\\Users\\pc\\Documents\\GitHub\\Dictionary-app-using-tkinter-in-Python\\galaxy.jpg"))
+Label(window,image=img).place(x = 0, y = 0)
 
-e=Entry(window,fg="black",bg="ivory3",font="Comic 18",width=20, borderwidth= 2)
+e = Entry(window,fg="black",bg="ivory3",font="Comic 18",width=20, borderwidth= 2)
 e.pack(side= TOP, anchor= NW, padx= 15, pady= 20)
 
 meaning = Label(window, text = "Meaning of the word will appear here", wraplength = 400, fg = "white", bg = "black", font = "Helvetica 17", justify = "left", width= 45, height= 14)
@@ -26,8 +25,9 @@ def click():
     word = e.get().strip()
     e.delete(0,END)
     global meaning
+    global flag
 
-    if word== "":
+    if word == "":
         meaning.config(text= "Please enter a word!")
 
     else:
@@ -36,40 +36,36 @@ def click():
             dict = PyDictionary()
             ansDict = dict.meaning(word)
             print(ansDict, "\n\n")
-            ans = "Noun:\n" + ansDict["Noun"][0]
+            ans = "Noun:\n1) " + str(ansDict["Noun"][0]) + "."
             if len(ansDict["Noun"]) > 1:
-                ans += "\n" + ansDict["Noun"][1]
+                ans += "\n2) " + str(ansDict["Noun"][1]) + "."
             if "Verb" in ansDict:
-                ans = ans + "\n\nVerb:\n" + ansDict["Verb"][0]
-            
+                ans += "\n\nVerb:\n" + str(ansDict["Verb"][0]) + "."
+            print(ans, "\n\n")
             meaning.config(text = ans)
-            
+
             imgBaseUrl = F"https://api.unsplash.com/search/photos?query={word}&client_id=Evn8ALswgo93-PUHSwAOyD08lOFDyKX-I50TUbuCin0"
             imgData = requests.get(imgBaseUrl).json()
             
             imgLink = imgData["results"][0]["urls"]["small"]
-            
-            print(imgLink)
-            html = F'''<img src="{imgLink}" alt="Image of {word}" width = 300 height = 300>'''
 
-            imgTag = HTMLLabel(window, html = html)            
+            html = F'''<img src="{imgLink}" alt="Image of {word}" width = 300 height = 320>'''
+
+
+            imgTag = HTMLLabel(window, html = html)
             imgTag.pack(fill = "both", expand = True)
-            
 
-            data = requests.get(base_url + word).json()
-            sound= 'https:' + data[0]["phonetics"][0]["audio"]
-            playsound(sound)
-            print(sound)
+
 
         except:
             meaning.config(text= "There was an error! Please make sure that the spelling is correct and you're connected to the internet!")
 
-search = ImageTk.PhotoImage(Image.open("searchIcon.jpg"))
+
+search = ImageTk.PhotoImage(Image.open("C:\\Users\\pc\\Documents\\GitHub\\Dictionary-app-using-tkinter-in-Python\\searchIcon.jpg"))
 Button(window, image = search, command = click).place(x= 290, y= 21)
 
 
 window.bind("<Return>",lambda event=None: click() )
-
 
 
 window.mainloop()
